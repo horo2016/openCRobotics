b002c5b41a7396aae7e9fae6be04e7e903b612b0
horo2016 <selectld@163.com>
2022-02-22 14:25:29 +0800

    Create main.c

---
+++ mqtt_waypointsave/src/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+/*pthread*/
+#include "sys/ipc.h"
+#include "sys/msg.h"
+#include "pthread.h"
+#include <arpa/inet.h>
+#include <net/if.h>
+
+
+/*Mqtt about*/
+
+#include "mqtt_main.h"
+/*******************************************************************************
+* function name	: main
+* description	: main function for control_engine
+* param[in] 	: none
+* param[out] 	: none
+* return 		: 0-success,-1-fail
+*******************************************************************************/
+int  main (int argc, char ** argv)
+{
+
+
+	pthread_attr_t attr;
+    pthread_t pthread_id = 0 ;
+	struct sched_param param;
+    /*create task mqtt  com queue */
+	pthread_attr_init (&attr);
+	pthread_attr_setschedpolicy (&attr, SCHED_RR);
+	param.sched_priority = 5;
+	pthread_attr_setschedparam (&attr, &param);
+	pthread_create (&pthread_id, &attr, &Mqtt_ClentTask, NULL);
+	pthread_attr_destroy (&attr);
+	  /*create task mqtt  client queue */
+	  /*
+	pthread_attr_init (&attr);
+	pthread_attr_setschedpolicy (&attr, SCHED_RR);
+	param.sched_priority = 5;
+	pthread_attr_setschedparam (&attr, &param);
+	pthread_create (&pthread_id, &attr, &Mqtt_ClentTask, NULL);
+	pthread_attr_destroy (&attr);*/
+    Mqtt_PublishTask(NULL);
+}
